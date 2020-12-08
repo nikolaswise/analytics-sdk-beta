0b79c236bbd7570ba2f8308bc8f731cb69413704
nikolaswise <nikolaswise@gmail.com>
2020-12-08 13:31:45 -0800

    basics

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-export default Event = (data) => {
-  console.log(data)
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response
 }
+
+const event = (data) => {
+  console.log('post data to localhost:3000/api/event')
+  console.log(data)
+  postData('http://localhost:3000/api/event', data)
+    .then(r => {
+      console.log('post complete')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+exports.event = event;
